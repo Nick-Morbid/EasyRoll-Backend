3c1af6b7f0ba68e55442df24ef6f74fd533d602c
Nick <1328530188@qq.com>
2022-11-07 19:53:02 +0800

    feat:完成对后端基础框架的构建

---
+++ Api/target/generated-sources/annotations/com/system/roll/mapstruct/UserConvertorImpl.java
@@ -0,0 +1,46 @@
+package com.system.roll.mapstruct;
+
+import com.system.roll.constant.impl.Role;
+import com.system.roll.entity.pojo.User;
+import com.system.roll.entity.vo.UserVo;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-11-07T19:50:26+0800",
+    comments = "version: 1.4.1.Final, compiler: javac, environment: Java 15 (Oracle Corporation)"
+)
+@Component
+public class UserConvertorImpl implements UserConvertor {
+
+    @Override
+    public UserVo UserToUserVo(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserVo userVo = new UserVo();
+
+        userVo.setUserId( user.getId() );
+        userVo.setUserName( user.getName() );
+        userVo.setRoleName( userRoleMsg( user ) );
+
+        return userVo;
+    }
+
+    private String userRoleMsg(User user) {
+        if ( user == null ) {
+            return null;
+        }
+        Role role = user.getRole();
+        if ( role == null ) {
+            return null;
+        }
+        String msg = role.getMsg();
+        if ( msg == null ) {
+            return null;
+        }
+        return msg;
+    }
+}
---
+++ Web/src/main/java/roll/EasyRollStarter.java
@@ -0,0 +1,11 @@
+package roll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyRollStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(EasyRollStarter.class,args);
+    }
+}
