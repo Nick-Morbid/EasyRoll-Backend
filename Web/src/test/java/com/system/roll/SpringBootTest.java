1291d4932f3fb7374d3ada91722a4806595c95e8
Nick <1328530188@qq.com>
2022-11-14 00:59:45 +0800

    fix:删去了一些关键信息

--- Web/src/test/java/com/system/roll/SpringBootTest.java
+++ Web/src/test/java/com/system/roll/SpringBootTest.java
@@ -1,14 +1,12 @@
 package com.system.roll;
 
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.system.roll.entity.vo.Result;
 import com.system.roll.security.jwt.JwtSecurityHandler;
 import com.system.roll.uitls.HttpRequestUtil;
 import org.junit.jupiter.api.Test;
 
 import javax.annotation.Resource;
 import java.io.IOException;
-import java.util.Map;
 
 @org.springframework.boot.test.context.SpringBootTest
 public class SpringBootTest {
@@ -34,15 +32,6 @@ public class SpringBootTest {
     private HttpRequestUtil httpRequestUtil;
     @Test
     public void testHttpGet() throws IOException {
-        String appid = "wxe06234d727d0baf0";
-        String secret = "275855de1e79658b0a0921d3a96cce6b";
-        String grant_type = "authorization_code";
-        String js_code = "053ro50009YiUO1AVn100tiGV31ro50Y";
-        Result<?> result = httpRequestUtil.httpGet("https://api.weixin.qq.com/sns/jscode2session", Map.of("appid", appid, "secret", secret, "grant_type", grant_type, "js_code", js_code));
-        System.out.println(result.getStatus());
-        System.out.println(result.getMessage());
-        Map<String,Object> data = (Map<String, Object>) result.getData();
-        System.out.println(data.get("session_key"));
-        System.out.println(data.get("openid"));
+
     }
 }
