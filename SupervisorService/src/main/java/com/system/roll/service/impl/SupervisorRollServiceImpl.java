4b56e877d3e44ae050f70768312a129c7304e85f
Nick <1328530188@qq.com>
2022-11-17 17:47:41 +0800

    fix:取消了敏感信息的上传

--- SupervisorService/src/main/java/com/system/roll/service/impl/SupervisorRollServiceImpl.java
+++ SupervisorService/src/main/java/com/system/roll/service/impl/SupervisorRollServiceImpl.java
@@ -1,8 +1,13 @@
 package com.system.roll.service.impl;
 
+import com.system.roll.controller.SupervisorRollController;
 import com.system.roll.service.SupervisorRollService;
 import org.springframework.stereotype.Component;
 
 @Component(value = "supervisorRollService")
 public class SupervisorRollServiceImpl implements SupervisorRollService {
+    @Override
+    public void publishRoll(SupervisorRollController.RollDto data) {
+
+    }
 }
