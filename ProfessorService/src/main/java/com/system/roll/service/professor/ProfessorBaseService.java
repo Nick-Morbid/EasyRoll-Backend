caa96f207c472b6abcedaceb98aa30db8ae23352
Nick <1328530188@qq.com>
2022-11-17 17:33:04 +0800

    feat(SupervisorBaseController.java,SupervisorInfoController.java):增加了督导人员端的接口定义

--- ProfessorService/src/main/java/com/system/roll/service/professor/ProfessorBaseService.java
+++ ProfessorService/src/main/java/com/system/roll/service/professor/ProfessorBaseService.java
@@ -1,4 +1,17 @@
 package com.system.roll.service.professor;
 
+import com.system.roll.controller.professor.ProfessorBaseController;
+import com.system.roll.entity.vo.course.CourseListVo;
+import com.system.roll.entity.vo.course.CourseVo;
+import org.springframework.web.multipart.MultipartFile;
+
 public interface ProfessorBaseService {
+
+    CourseVo uploadCourse(ProfessorBaseController.CourseDto courseDto, MultipartFile studentList);
+
+    void updateCourse(ProfessorBaseController.CourseDto courseDto, MultipartFile studentList);
+
+    void deleteCourse(String courseId);
+
+    CourseListVo getCourseList(String courseId);
 }
--- SupervisorService/src/main/java/com/system/roll/service/SupervisorRollService.java
+++ SupervisorService/src/main/java/com/system/roll/service/SupervisorRollService.java
@@ -1,4 +1,13 @@
 package com.system.roll.service;
 
+import com.system.roll.controller.SupervisorRollController;
+
 public interface SupervisorRollService {
+
+    /**
+     * 发布定位签到
+     * */
+    void publishRoll(SupervisorRollController.RollDto data);
+
+
 }
