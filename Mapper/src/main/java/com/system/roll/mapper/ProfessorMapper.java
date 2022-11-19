20c35ba0532dd5631c7ae35c85b8e2e9346c1ae9
wangxintao2002 <2454381779@qq.com>
2022-11-19 23:04:58 +0800

    feat:新增ProfessorMapper

---
+++ Mapper/src/main/java/com/system/roll/mapper/ProfessorMapper.java
@@ -0,0 +1,9 @@
+package com.system.roll.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.system.roll.entity.pojo.Professor;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface ProfessorMapper extends BaseMapper<Professor> {
+}
